dbcc3f16444f033b2a0db8a2e232bfabe11d9201
Joanna Nodzynska <joanna.nodzynska@gmail.com>
2020-05-17 14:19:19 +0200

    FrontEnd updated for registration

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,7 +11,19 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+	  <button @click="registering = false"
+	          :class="registering ? 'button-outline' : ''">Loguję się</button>
+	  <button @click="registering = true"
+	          :class="!registering ? 'button-outline' : ''">Rejestruję się</button>
+	
+		<div v-if="error" class="error-alert">{{ error }}</div>
+	
+      <login-form @login="login($event)"
+	              v-if="registering == false"></login-form>
+	  
+	  <login-form @login="register($event)"
+	              v-else
+	              button-label="Zarejestruj się"></login-form>
     </div>
   </div>
 </template>
@@ -20,18 +32,29 @@
     import "milligram";
     import LoginForm from "./LoginForm";
     import MeetingsPage from "./meetings/MeetingsPage";
-
     export default {
         components: {LoginForm, MeetingsPage},
         data() {
             return {
-                authenticatedUsername: ""
+                authenticatedUsername: "",
+				registering: false,
+				error: ''
             };
         },
         methods: {
             login(user) {
                 this.authenticatedUsername = user.login;
             },
+			register(user) {
+			    this.error = '';
+				this.$http.post('participants', user)
+				 .then(response => {
+					 this.registering = false;
+				 })
+				 .catch(response => {
+					 this.error = "Użytkownik już istnieje!";     
+				 });
+			},
             logout() {
                 this.authenticatedUsername = '';
             }
@@ -44,9 +67,16 @@
     max-width: 1000px;
     margin: 0 auto;
   }
-
   .logo {
     vertical-align: middle;
   }
-</style>
+  
+  .error-alert {
+    border: 3px dotted red;
 
+	padding: 10px;
+	background: pink;
+	text-align: center;
+	
+  }
+</style>
